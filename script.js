38dd1f579b3536027fef26a33a608158c5978688
Isaadr <i.mosquerad@uniandes.edu.co>
2026-02-23 01:36:47 +0000

    Cuento

--- script.js
+++ script.js
@@ -3,12 +3,23 @@
 const titulo = document.getElementById("titulo");
 const texto = document.getElementById("texto");
 
-const textos = ["uno", "dos", "tres", "cuatro", "cinco"];
-console.log(textos[0]);
+const textos = ["Hace mucho tiempo", "Existia el vacio", "Todo era oscuro", "Hasta que la luz fue creada", "y el agua empezo a fluir", "Los animales fueron puestos en la tierra", "flores, arboles y rocas", "todo fue creado", "Y existio...", "El Edén" ];
+
 let textoOriginal = true; //variable de tipo booleano: dos valores verdadero o falso
 let iTxt = 0;
 
-console.log(titulo);
+if(undefined === true) {
+
+}else {
+    texto.innerText = "Hace mucho tiempo"
+}
+
+
+
+
+
+
+console.log(texto);
 
 titulo.addEventListener("click", () => {
     console.log("hiciste clik");
@@ -18,8 +29,11 @@ titulo.addEventListener("click", () => {
         
     } else {
         titulo.innerText = "antes de todo";
-    } 
+    }  
     textoOriginal = !textoOriginal;
+
+   
+
 
     
     //alert("hiciste click");
@@ -29,6 +43,7 @@ titulo.addEventListener("click", () => {
 texto.addEventListener("click", () => {
     texto.innerText = textos [iTxt];
     iTxt += 1; 
+    
 
 })
 
